5c4d4fc276af59c88014c55859dcad9b7416a89d
zeke177 <98616230+zeke177@users.noreply.github.com>
2022-01-28 13:08:42 -0700

    Add files via upload

---
+++ src/app/app-wrapper/app-wapper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper',
+  templateUrl: './app-wrapper.component.html',
+  styleUrls: ['./app-wrapper.component.scss']
+})
+export class AppWrapperComponent {
+}

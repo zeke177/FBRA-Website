ff4d6c6deb34f81234a3795715f4cd7559b9da3d
zeke177 <98616230+zeke177@users.noreply.github.com>
2022-01-28 13:12:58 -0700

    Add files via upload

---
+++ src/app/tab-two/tab-two.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-two',
+  templateUrl: './tab-two.component.html',
+  styleUrls: ['./tab-two.component.scss']
+})
+export class TabTwoComponent {
+    
+}
